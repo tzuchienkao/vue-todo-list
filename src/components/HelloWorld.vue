fb07f02bc6953f65389fb6d511cc8fc558971ef6
tzuchienkao <oyan114@gmail.com>
2019-09-02 23:30:27 +0800

    restore file

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,106 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
+<div>
+  <header>
+    <h2>午茶時光</h2>
     <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
+      我想要點...
+      <input type="text">
     </p>
-    <h3>Installed CLI Plugins</h3>
+  </header>
+  <section>
     <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
+      <li @click="sortBy('all')">
+        <a href="javascript:void(0)">所有訂單 ({{ totalSum }})</a>
+      </li>
+      <li @click="sortBy('nodo')">
+        <a href="javascript:void(0)">未完成 ({{ nodoSum }})</a>
+      </li>
+      <li @click="sortBy('done')">
+        <a href="javascript:void(0)">已完成 ({{ doneSum }})</a>
+      </li>
     </ul>
-    <h3>Essential Links</h3>
     <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
+      <li v-for="(item, index) in list">
+        <template>
+          <input type="text" v-input-focus>
+        </template>
+        <template>
+          <div>
+            <input type="checkout" />
+            <label for="">{{ item.item }}</label>
+          </div>
+          <div>
+            <button>修改</button>
+            <button>結單</button>
+          </div>
+        </template>
+      </li>
     </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  </section>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  name: 'todoList',
+  data() {
+    return {
+      sort: 'all',
+      orderData: [{
+        item: '綠茶',
+        done: true
+      }, {
+        item: '紅茶',
+        done: true
+      }, {
+        item: '綠茶',
+        done: false
+      }]
+    }
+  },
+  computed: {
+    list: function () {
+      let type = this.sort;
+      switch (type) {
+        case 'all':
+          return this.orderData;
+        case 'nodo':
+          return this.orderData.filter(v => {
+            return !v.done;
+          });
+        case 'done':
+          return this.orderData.filter(v => {
+            return v.done;
+          });
+      }
+    },
+    totalSum: function () {
+      return this.orderData.length;
+    },
+    nodoSum: function () {
+      let nodo = this.orderData.filter(v => {
+        return !v.done;
+      });
+      return nodo.length;
+    },
+    doneSum: function () {
+      let done = this.orderData.filter(v => {
+        return v.done;
+      });
+      return done.length;
+    }
+  },
+  methods: {
+    sortBy (type) {
+      this.sort = type;
+    }
+  },
+  directives :{
+      inputFocus :{ //自訂標籤名稱
+          inserted: function(el){
+              el.focus();
+          }
+      }
   }
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
